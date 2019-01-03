d4cab542973c4c75d88972894883c691a02563bb
Jardetzky <max.jardetzky20@bcp.org>
2019-01-03 07:51:27 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,155 @@
+// main.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Client records a client connecting to the server.
+type Client struct {
+	conn *websocket.Conn
+}
+
+// ClientList binds clients with a mutex structure to prevent race conditions.
+type ClientList struct {
+	mutex   *sync.Mutex
+	clients []*Client
+}
+
+// Grid holds the color values for each square.
+type Grid struct {
+	mutex  *sync.Mutex
+	colors []string
+}
+
+var srv *http.Server
+var clientList ClientList
+var grid Grid
+var gridSize int
+var gridColor string
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	srv = &http.Server{Addr: ":80"}
+	gridSize = 13 * 13
+	gridColor = "rgb(99, 110, 114)"
+	clientList = ClientList{
+		mutex:   &sync.Mutex{},
+		clients: make([]*Client, 0),
+	}
+	grid = Grid{
+		mutex:  &sync.Mutex{},
+		colors: make([]string, gridSize),
+	}
+	fmt.Println("Startup successful! Type 'clear' to clear board.")
+	go launchCLI()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/draw", launchHTTP)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func launchHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err == nil {
+		client := &Client{conn}
+		clientList.mutex.Lock()
+		clientList.clients = append(clientList.clients, client)
+		clientList.mutex.Unlock()
+		defer clientList.deleteClient(client)
+
+		for i, v := range grid.colors {
+			if len(v) > 0 {
+				clientList.sendClient(client, strconv.Itoa(i)+" "+v)
+			}
+		}
+
+		for {
+			// Read message from browser
+			_, msg, err := conn.ReadMessage()
+			if len(msg) > 0 {
+				fmt.Println(string(msg))
+				grid.mutex.Lock()
+				splitMsg := strings.SplitN(string(msg), " ", 2)
+				if index, err := strconv.Atoi(splitMsg[0]); err == nil {
+					grid.colors[index] = splitMsg[1]
+				} else {
+					panic(err)
+				}
+				grid.mutex.Unlock()
+				clientList.sendAll(string(msg))
+			}
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+func launchCLI() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		switch scanner.Text() {
+		case "clear":
+			grid.mutex.Lock()
+			grid.colors = make([]string, gridSize)
+			for i := range grid.colors {
+				clientList.sendAll(strconv.Itoa(i) + " " + gridColor)
+			}
+			grid.mutex.Unlock()
+			fmt.Println("board cleared")
+		default:
+			fmt.Println("invalid command", scanner.Text())
+		}
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+}
+
+func (clientList *ClientList) sendClient(client *Client, msg string) {
+	if err := client.conn.WriteMessage(1, []byte(msg)); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (clientList *ClientList) sendAll(msg string) {
+	defer clientList.mutex.Unlock()
+	clientList.mutex.Lock()
+	for _, v := range clientList.clients {
+		clientList.sendClient(v, msg)
+	}
+}
+
+func (clientList *ClientList) deleteClient(client *Client) {
+	defer clientList.mutex.Unlock()
+	clientList.mutex.Lock()
+	for i, v := range clientList.clients {
+		if reflect.DeepEqual(client, v) {
+			clientList.clients = append(clientList.clients[:i], clientList.clients[i+1:]...)
+			break
+		}
+	}
+}
---
+++ static/js/index.js
@@ -0,0 +1,90 @@
+// index.js
+var windowAddr = window.location.href.substring(window.location.href.indexOf("/", window.location.href.indexOf("/") + 1) + 1, window.location.href.length);
+var socket;
+var colors = ["rgb(235, 59, 90)", "rgb(250, 130, 49)", "rgb(254, 211, 48)", "rgb(32, 191, 107)", "rgb(56, 103, 214)", "rgb(136, 84, 208)"];
+var color = "rgb(235, 59, 90)"; // ^ German Palette for picker, American Palette for background (FlatUIColors)
+var rainbow = false;
+var random = false;
+var counter = 0;
+openSocket();
+
+var container = document.getElementById("grid");
+var counter = 0;
+for (var i = 0; i < 13; i++) {
+    var row = document.createElement("div");
+    row.className = "row gridRow";
+    for (var j = 0; j < 13; j++) {
+        var col = document.createElement("div");
+        col.className = "col gridCol"; 
+        col.id = counter.toString();
+        col.style.backgroundColor = "rgb(99, 110, 114)";
+        counter++;
+        row.appendChild(col);
+    }
+    container.appendChild(row);
+}
+
+var pickerCols = document.getElementsByClassName("pickerCol");
+for (var i = 0; i < pickerCols.length; i++) {
+    pickerCols[i].onclick = function() {
+        for (var j = 0; j < pickerCols.length; j++) {
+            pickerCols[j].className = "col pickerCol unselected";
+        }
+        this.className = "col pickerCol selected";
+        if (this.id == "rainbow") {
+            rainbow = true;
+            random = false;
+        } else if (this.id == "random") {
+            random = true;
+            rainbow = false;
+        } else {
+            color = getComputedStyle(this).color;
+            rainbow = false;
+            random = false;
+        }
+    }
+}
+
+var gridCols = document.getElementsByClassName("gridCol");
+for (var i = 0; i < gridCols.length; i++) {
+    gridCols[i].onclick = function() {
+        draw(this.id);
+    }
+}
+
+document.addEventListener("mousedown", function(event){
+    for (var i = 0; i < gridCols.length; i++) {
+        gridCols[i].onmouseover = function() {
+            draw(this.id);
+        }
+    }
+}, false);
+
+document.addEventListener("mouseup", function(event){
+    for (var i = 0; i < gridCols.length; i++) {
+        gridCols[i].onmouseover = function(){};
+    }
+}, false);
+
+function openSocket() {
+    socket = new WebSocket("ws://" + windowAddr + "draw");
+
+    socket.onmessage = function(e) {
+        var data = e.data.match(/^(\S+)\s(.*)/).slice(1);
+        document.getElementById(data[0]).style.backgroundColor = data[1];
+    };
+}
+
+function draw(id) {
+    if (rainbow) {
+        socket.send(id + " " + colors[counter]);
+        counter++;
+        if (counter > 6) {
+            counter = 0;
+        }
+    } else if (random) {
+        socket.send(id + " " + colors[Math.floor(Math.random()*colors.length)]);
+    } else {
+        socket.send(id + " " + color);
+    }
+}
